09ae55e3ac8095fd7dd6cf0ac3fceefe9f999833
Abhishek <abhics33@gmail.com>
2019-02-09 19:02:35 +0530

    Hello Name Program

---
+++ src/MainClass.java
@@ -0,0 +1,11 @@
+package CoreJava;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello My name is Abhishek Pratap");
+	}
+
+}
